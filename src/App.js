cca130a76a2a9770b7ebee6403cc4a703d89c1bc
noobapoo99 <apoorvtripathi99@gmail.com>
2024-08-13 23:08:13 +0530

    navbar-using-daisyui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header.js";
+import Header from "./components/Header.jsx";
 import Home from "./components/Home.js";
 import Cart from "./components/Cart.js";
 
