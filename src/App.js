338fbdc3633538397f71e395c7288ddd7eda72fe
noobapoo99 <apoorvtripathi99@gmail.com>
2024-08-14 02:10:35 +0530

    remove_useState_from_app_to_implement_context

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Cart from "./components/Cart.js";
 import { useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState([]);
-
   return (
     <BrowserRouter>
       <Header />
