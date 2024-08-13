5917b1679942f1309566115d7e0c5d4558f1e957
noobapoo99 <apoorvtripathi99@gmail.com>
2024-08-14 02:09:27 +0530

    value_used_in_context

--- src/Context.js
+++ src/Context.js
@@ -1,8 +1,9 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 const Cart = createContext();
 
 const Context = ({ children }) => {
-  return <Cart.Provider>{children}</Cart.Provider>;
+  const [cart, setCart] = useState([]);
+  return <Cart.Provider value={{ cart, setCart }}>{children}</Cart.Provider>;
 };
 
 export default Context;
